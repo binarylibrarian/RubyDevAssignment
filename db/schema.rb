0d3b06c74e3dfe4bb9f9b3479329e4c049c7496d
Ian Strang <ian.strang@gmail.com>
2016-08-31 13:42:52 -0400

    Added location model for tracking event locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831173451) do
+ActiveRecord::Schema.define(version: 20160831174144) do
 
   create_table "events", force: :cascade do |t|
     t.integer  "user_id"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20160831173451) do
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id"
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
