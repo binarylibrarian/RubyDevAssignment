67a169efd8aad1200292921cd89af63307626cb1
Ian Strang <ian.strang@gmail.com>
2016-09-02 16:29:29 -0400

    Added fixtures for events, users locations etc

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "search should return a elasticsearch resuts" do
+    result = Event.search 'music'
+    assert_not_nil result
+  end
+
+  test "search should return resuts" do
+    result = Event.search 'music'
+    assert result.count > 0
+  end
+
+  test "event should have messages from user" do
+    result = Event.where('messages @> ?', '[{"user_id":1}]')
+    assert result.count > 0
+  end
+
+  test "event should have multiple organizers" do
+    e = Event.first
+    assert e.organizer.count > 1
+  end
+
 end
