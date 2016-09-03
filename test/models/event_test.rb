4d8cb41c65374b4f5efc13796beac92544b7c7d4
Ian Strang <ian.strang@gmail.com>
2016-09-03 14:35:11 -0400

    Added test for event filtering

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
+
+  # Search test
+
   test "search should return a elasticsearch resuts" do
     result = Event.search 'music'
     assert_not_nil result
@@ -15,6 +18,8 @@ class EventTest < ActiveSupport::TestCase
     end
   end
 
+  # Messages and multiuser tests
+
   test "event should have messages from user" do
     result = Event.where('messages @> ?', '[{"user_id":1}]')
     assert result.count > 0
@@ -23,6 +28,36 @@ class EventTest < ActiveSupport::TestCase
   test "event should have multiple organizers" do
     e = Event.first
     assert e.organizer.count > 1
+  end
+
+  # Filter test
+
+  test "events filtered by location should be fewer then total events" do
+    total = Event.all.count
+    toronto_total = Event.location("Toronto").count
+    assert total > toronto_total
+  end
+
+  test "events filtered by location should have that location" do
+    assert Event.location("Toronto").include?("Toronto")
+
+  end
+
+  test "events filtered by topic should be less then total" do
+    total = Event.all.count
+    music_total = Event.topics("music").count
+    assert total > music_total
+  end
+
+  test "event filtered by topic should contain that topic" do
+    assert Event.topics("music").first.topics.include?("music")
+  end
+
+  test "events filtered by date should be fewer then total" do
+    total = Event.all.count
+    starttime = '2016-08-31'.to_datetime
+    endtime = '2016-09-10'.to_datetime
+    assert total > Event.where(start_at: starttime...endtime)
   end
 
 end
